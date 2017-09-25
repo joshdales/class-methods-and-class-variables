42ff6aab1a9de45c5668b1e837cca32f33e7ab72
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-09-25 17:28:24 -0400

    Fix due-date

--- book.rb
+++ book.rb
@@ -1,6 +1,7 @@
 class Book
   @@on_shelf = []
   @@on_loan = []
+  @@overdue = []
 
   def initialize(title, author, isbn)
     @on_loan = false
@@ -74,10 +75,10 @@ class Book
     @@on_shelf.sample
   end
 
-  def self.overdue_books
+  def self.overdue
     @@on_loan.each do |book|
-      if @due_date < Time.now + (2*7*24*60*60)
-        return book
+      if book.due_date < Time.now
+         @@overdue << book
       end
     end
   end
@@ -89,16 +90,20 @@ sister_outsider = Book.create("Sister Outsider", "Audre Lorde", "9781515905431")
 aint_i = Book.create("Ain't I a Woman?", "Bell Hooks", "9780896081307")
 if_they_come = Book.create("If They Come in the Morning", "Angela Y. Davis", "0893880221")
 puts Book.on_shelf?
-
 puts Book.browse.inspect
 puts Book.browse.inspect
 puts Book.browse.inspect
-
-aint_i.borrow
-puts Book.on_shelf?
-puts aint_i.inspect
-
-puts aint_i.due_date
-aint_i.return_to_libary
-puts sister_outsider.inspect
-puts aint_i.inspect
+puts Book.borrowed.inspect
+puts sister_outsider.lent_out?
+puts sister_outsider.borrow
+puts sister_outsider.lent_out?
+puts sister_outsider.borrow
+puts sister_outsider.due_date
+puts Book.available.inspect
+puts Book.borrowed.inspect
+puts Book.overdue.inspect # []
+puts"---"
+puts sister_outsider.return_to_library # true
+puts sister_outsider.lent_out? # false
+puts Book.available.inspect # [#<Book:0x00562314676118 @title="Ain't I a Woman?", @author="Bell Hooks", @isbn="9780896081307">, #<Book:0x00562314675fd8 @title="If They Come in the Morning", @author="Angela Y. Davis", @isbn="0893880221">, #<Book:0x00562314676208 @title="Sister Outsider", @author="Audre Lorde", @isbn="9781515905431", @due_date=nil>]
+puts Book.borrowed.inspect # []
