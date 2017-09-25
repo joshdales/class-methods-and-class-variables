7034151d10cff95e36e93f5d7075758f080444fe
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-09-25 19:48:45 -0400

    Set class methods for drink_blood and go_home

--- vampire.rb
+++ vampire.rb
@@ -1,6 +1,16 @@
 class Vampire
-@name
-@age
-@in_coffin = true
-@drank_blood_today = true
+  @name
+  @age
+  @@in_coffin = true
+  @@drank_blood_today = true
+
+  def drink_blood
+    @@drank_blood_today = true
+  end
+
+  def go_home
+    @@in_coffin = true
+  end
+
+
 end
