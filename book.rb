bc2ff56c943fc216c305d423450841eddc184a7e
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-09-25 16:57:23 -0400

    Instance methods for borrow and return_to_libary

--- book.rb
+++ book.rb
@@ -4,37 +4,68 @@ class Book
 
   def initialize
     @on_loan = false
+    @due_date = nil
   end
 
   def borrow
-    @on_loan = true
+    if @on_loan == false
+      @due_date = Book.current_due_date
+      @@on_shelf.delete(self)
+      @@on_loan << self
+      @on_loan = true
+    else
+      false
+    end
   end
 
   def return_to_libary
-    @on_loan = false
+    if @on_loan
+      @due_date = nil
+      @@on_loan.delete(self)
+      @@on_shelf << self
+      @on_loan = false
+    else
+      false
+    end
   end
 
   def lent_out?
     if @on_loan
-      puts "This book is on loan."
+      true
     else
-      puts "This book is in stock"
+      false
     end
   end
 
   def due_date
-    #2 weeks from now
+    Time.at(@due_date)
+  end
+
+  def self.create
+    new_book = Book.new
+    @@on_shelf << new_book
+    new_book
+  end
+
+  def self.current_due_date
     Time.now + (2*7*24*60*60)
+  end
+
+  def self.on_loan?
+    @@on_loan
+  end
+
+  def self.on_shelf?
+    @@on_shelf
   end
 
 end
 
-book = Book.new
-
-puts book.lent_out?
-puts book.borrow
-puts book.lent_out?
-puts book.return_to_libary
-puts book.lent_out?
+book = Book.create
+book.borrow
+puts Book.on_shelf?
+puts book.inspect
 
 puts book.due_date
+book.return_to_libary
+puts book.inspect
