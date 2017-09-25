c8cbff566bf566c6d56613b3e58f4f0e6cf5ba51
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-09-25 17:36:57 -0400

    Fix spelling of library

--- book.rb
+++ book.rb
@@ -22,10 +22,11 @@ class Book
     end
   end
 
-  def return_to_libary
+  def return_to_library
     if @on_loan
       @due_date = nil
       @@on_loan.delete(self)
+      @@overdue.delete(self)
       @@on_shelf << self
       @on_loan = false
     else
@@ -101,9 +102,8 @@ puts sister_outsider.borrow
 puts sister_outsider.due_date
 puts Book.available.inspect
 puts Book.borrowed.inspect
-puts Book.overdue.inspect # []
-puts"---"
-puts sister_outsider.return_to_library # true
-puts sister_outsider.lent_out? # false
-puts Book.available.inspect # [#<Book:0x00562314676118 @title="Ain't I a Woman?", @author="Bell Hooks", @isbn="9780896081307">, #<Book:0x00562314675fd8 @title="If They Come in the Morning", @author="Angela Y. Davis", @isbn="0893880221">, #<Book:0x00562314676208 @title="Sister Outsider", @author="Audre Lorde", @isbn="9781515905431", @due_date=nil>]
-puts Book.borrowed.inspect # []
+puts Book.overdue.inspect
+puts sister_outsider.return_to_library
+puts sister_outsider.lent_out?
+puts Book.available.inspect
+puts Book.borrowed.inspect
