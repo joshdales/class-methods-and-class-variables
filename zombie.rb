fa4f9a3fb700f8c930df2e44ad4e8e11a272f7e4
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-09-25 17:51:57 -0400

    Initialize zombie skills

--- zombie.rb
+++ zombie.rb
@@ -6,4 +6,17 @@ class Zombie
   @@default_speed = 1
   @@default_strength = 3
 
+  def initialize(zombie_speed, zombie_strength)
+    if zombie_speed > @@max_speed
+      @zombie_speed = @@default_speed
+    else
+      @zombie_speed = zombie_speed
+    end
+
+    if zombie_strength > @@max_strength
+      @zombie_strength = @@default_strength
+    else
+      @zombie_strength = zombie_strength
+  end
+
 end
