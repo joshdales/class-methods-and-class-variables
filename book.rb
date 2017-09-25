269a22427b07a98e386b1a01df8f5d3d044f9add
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-09-25 15:45:47 -0400

    Create instance methods tp lend out and return books, and check if on loan

--- book.rb
+++ book.rb
@@ -2,13 +2,39 @@ class Book
   @@on_shelf = []
   @@on_loan = []
 
+  def initialize
+    @on_loan = false
+  end
+
+  def borrow
+    @on_loan = true
+  end
+
+  def return_to_libary
+    @on_loan = false
+  end
+
+  def lent_out?
+    if @on_loan
+      puts "This book is on loan."
+    else
+      puts "This book is in stock"
+    end
+  end
+
   def due_date
     #2 weeks from now
     Time.now + (2*7*24*60*60)
   end
-  
+
 end
 
 book = Book.new
+
+puts book.lent_out?
+puts book.borrow
+puts book.lent_out?
+puts book.return_to_libary
+puts book.lent_out?
 
 puts book.due_date
