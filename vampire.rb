ede88251d663afdb3e79192ef714a794d828c82f
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-09-25 20:02:52 -0400

    Create sunrise class method

--- vampire.rb
+++ vampire.rb
@@ -1,16 +1,53 @@
 class Vampire
   @name
   @age
-  @@in_coffin = true
-  @@drank_blood_today = true
+  @@in_coffin = false
+  @@drank_blood_today = false
+  @@coven = []
 
-  def drink_blood
+  def initialize(name, age)
+    @name = name
+    @age = age
+    @@coven << @self
+  end
+
+  def self.drink_blood
     @@drank_blood_today = true
   end
 
-  def go_home
+  def self.go_home
     @@in_coffin = true
   end
 
+  def self.sunset
+    @@drank_blood_today = false
+    @@in_coffin = false
+  end
+
+  def self.sunrise
+    if @@in_coffin == false && @@drank_blood_today == false
+      @@coven.delete(self)
+    end
+  end
 
 end
+
+vampire1 = Vampire.new("Viago", 379)
+vampire2 = Vampire.new("Vladislav", 862)
+vampire3 = Vampire.new("Deacon", 183)
+vampire4 = Vampire.new("Petyr", 8000)
+
+puts vampire1.inspect
+puts vampire2.inspect
+puts vampire3.inspect
+puts vampire4.inspect
+
+puts Vampire.go_home
+puts Vampire.drink_blood
+
+puts vampire1.inspect
+puts vampire2.inspect
+puts vampire3.inspect
+puts vampire4.inspect
+
+puts Vampire.sunset
